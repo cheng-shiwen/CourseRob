b3a7bd00b73755945530efd4621d7d4a51a10c54
cheng-shiwen <chengshiwen0103@gmail.com>
2014-09-26 20:01:28 +0800

    Added deleting course and swapping course

--- courserob.py
+++ courserob.py
@@ -16,6 +16,7 @@ urllib2.install_opener(opener)
 # account info, academic year and semester
 username = ''
 password = ''
+studentid = ''
 xnxq = ''       # e.g. '2013-2014-2'
 
 def login():
@@ -47,6 +48,12 @@ def login():
         if login_success.search(login_html):
             break
 
+def get_base_url(course_type):
+    if course_type not in ['bx', 'xx', 'rx', 'xwk']:
+        raise Exception('course_type error')
+    return 'http://zhjwxk.cic.tsinghua.edu.cn/xkBks.vxkBksXkbBs.do' if course_type in ['bx', 'xx', 'rx'] else \
+           'http://zhjwxk.cic.tsinghua.edu.cn/xkYjs.vxkYjsXkbBs.do'
+
 def is_rob_success(base_url, course_number, serial_number):
     yx_url = base_url + '?m=yxSearchTab&p_xnxq=' + xnxq + '&tokenPriFlag=yx'
     yx_html = urllib2.urlopen(yx_url).read()
@@ -54,16 +61,12 @@ def is_rob_success(base_url, course_number, serial_number):
     # f_out.write(yx_html)
     # f_out.close()
 
-    token_regex = re.compile('<span>' + course_number + '</span>[\s\S]*?<span >' + serial_number + '</span>')
-    token_match = token_regex.search(yx_html)
-    return True if token_match else False
+    success_regex = re.compile('<span>' + course_number + '</span>[\s\S]*?<span >' + serial_number + '</span>')
+    success_match = success_regex.search(yx_html)
+    return True if success_match else False
 
-def course_rob(course_type, course_number, serial_number):
-    if course_type not in ['bx', 'xx', 'rx', 'xwk']:
-        raise Exception('course_type error')
-
-    base_url = 'http://zhjwxk.cic.tsinghua.edu.cn/xkBks.vxkBksXkbBs.do' if course_type in ['bx', 'xx', 'rx'] else \
-               'http://zhjwxk.cic.tsinghua.edu.cn/xkYjs.vxkYjsXkbBs.do'
+def rob_course(course_type, course_number, serial_number):
+    base_url = get_base_url(course_type)
 
     course_url = base_url + '?m=' + course_type + 'Search&p_xnxq=' + xnxq + '&tokenPriFlag=' + course_type
     course_html = urllib2.urlopen(course_url).read()
@@ -71,24 +74,95 @@ def course_rob(course_type, course_number, serial_number):
     # f_out.write(course_html)
     # f_out.close()
 
+    remain_regex = re.compile('>\s*?' + course_number + '</td>[\s\S]*?>' + serial_number + '</td>[\s\S]*?</td>[\s\S]*?>(\d+)</td>')
+    remain_match = remain_regex.search(course_html)
+    remain_num = int(remain_match.group(1))
+    print 'course remain: ', course_type, course_number, serial_number, remain_num
+
+    if remain_num:
+        token_regex = re.compile('<input\s+?type="hidden"\s+?name="token"\s+?value="(.*?)">')
+        token_match = token_regex.search(course_html)
+        token_string = token_match.group(1)
+        print 'course token: ', course_type, course_number, serial_number, token_string
+
+        course_body = {
+            'bx': (('m', 'saveBxKc'), ('token', token_string), ('p_xnxq', xnxq), ('tokenPriFlag', 'bx'), ('p_bxk_id', '%s;%s;%s;' % (xnxq, course_number, serial_number))),
+            'xx': (('m', 'saveXxKc'), ('token', token_string), ('p_xnxq', xnxq), ('tokenPriFlag', 'xx'), ('p_xxk_id', '%s;%s;%s;' % (xnxq, course_number, serial_number))),
+            'rx': (('m', 'saveRxKc'), ('token', token_string), ('p_xnxq', xnxq), ('tokenPriFlag', 'rx'), ('p_rx_id', '%s;%s;%s;' % (xnxq, course_number, serial_number)), ('p_sort.asc1', 'true'), ('p_sort.asc2', 'true')),
+            'xwk': (('m', 'saveXwKc'), ('token', token_string), ('p_xnxq', xnxq), ('tokenPriFlag', 'xwk'), ('p_xwk_id', '%s;%s;%s;' % (xnxq, course_number, serial_number)))
+        }
+        course_req = urllib2.Request(base_url, urllib.urlencode(course_body[course_type]))
+        course_req_html = urllib2.urlopen(course_req).read()
+        # f_out = open(course_type + '_req.html', 'w')
+        # f_out.write(course_req _html)
+        # f_out.close()
+
+        return is_rob_success(base_url, course_number, serial_number)
+
+    return False
+
+def rob_course_with_swap(new_course_type, new_course_number, new_serial_number, old_course_type, old_course_number, old_serial_number):
+    new_base_url = get_base_url(new_course_type)
+    old_base_url = get_base_url(old_course_type)
+    if new_base_url != old_base_url:
+        raise Exception('base_url different error')
+    base_url = new_base_url = old_base_url
+
+    course_url = base_url + '?m=' + new_course_type + 'Search&p_xnxq=' + xnxq + '&tokenPriFlag=' + new_course_type
+    course_html = urllib2.urlopen(course_url).read()
+    # f_out = open(course_type + '_course.html', 'w')
+    # f_out.write(course_html)
+    # f_out.close()
+
+    remain_regex = re.compile('>\s*?' + new_course_number + '</td>[\s\S]*?>' + new_serial_number + '</td>[\s\S]*?</td>[\s\S]*?>(\d+)</td>')
+    remain_match = remain_regex.search(course_html)
+    remain_num = int(remain_match.group(1))
+    print 'course remain: ', new_course_type, new_course_number, new_serial_number, remain_num
+
+    if remain_num:
+        del_course(old_course_type, old_course_number, old_serial_number)
+
+        token_regex = re.compile('<input\s+?type="hidden"\s+?name="token"\s+?value="(.*?)">')
+        token_match = token_regex.search(course_html)
+        token_string = token_match.group(1)
+        print 'course token: ', new_course_type, new_course_number, new_serial_number, token_string
+
+        course_body = {
+            'bx': (('m', 'saveBxKc'), ('token', token_string), ('p_xnxq', xnxq), ('tokenPriFlag', 'bx'), ('p_bxk_id', '%s;%s;%s;' % (xnxq, new_course_number, new_serial_number))),
+            'xx': (('m', 'saveXxKc'), ('token', token_string), ('p_xnxq', xnxq), ('tokenPriFlag', 'xx'), ('p_xxk_id', '%s;%s;%s;' % (xnxq, new_course_number, new_serial_number))),
+            'rx': (('m', 'saveRxKc'), ('token', token_string), ('p_xnxq', xnxq), ('tokenPriFlag', 'rx'), ('p_rx_id', '%s;%s;%s;' % (xnxq, new_course_number, new_serial_number)), ('p_sort.asc1', 'true'), ('p_sort.asc2', 'true')),
+            'xwk': (('m', 'saveXwKc'), ('token', token_string), ('p_xnxq', xnxq), ('tokenPriFlag', 'xwk'), ('p_xwk_id', '%s;%s;%s;' % (xnxq, new_course_number, new_serial_number)))
+        }
+        course_req = urllib2.Request(base_url, urllib.urlencode(course_body[new_course_type]))
+        course_req_html = urllib2.urlopen(course_req).read()
+        # f_out = open(course_type + '_req.html', 'w')
+        # f_out.write(course_req _html)
+        # f_out.close()
+
+        return is_rob_success(base_url, new_course_number, new_serial_number)
+
+    return False
+
+def del_course(course_type, course_number, serial_number):
+    base_url = get_base_url(course_type)
+
+    yx_url = base_url + '?m=yxSearchTab&p_xnxq=' + xnxq + '&tokenPriFlag=yx'
+    yx_html = urllib2.urlopen(yx_url).read()
+    # f_out = open('yx_course.html', 'w')
+    # f_out.write(yx_html)
+    # f_out.close()
+
     token_regex = re.compile('<input\s+?type="hidden"\s+?name="token"\s+?value="(.*?)">')
-    token_match = token_regex.search(course_html)
+    token_match = token_regex.search(yx_html)
     token_string = token_match.group(1)
     print course_type, course_number, serial_number, token_string
 
-    course_body = {
-        'bx': (('m', 'saveBxKc'), ('token', token_string), ('p_xnxq', xnxq), ('tokenPriFlag', 'bx'), ('p_bxk_id', '%s;%s;%s;' % (xnxq, course_number, serial_number))),
-        'xx': (('m', 'saveXxKc'), ('token', token_string), ('p_xnxq', xnxq), ('tokenPriFlag', 'xx'), ('p_xxk_id', '%s;%s;%s;' % (xnxq, course_number, serial_number))),
-        'rx': (('m', 'saveRxKc'), ('token', token_string), ('p_xnxq', xnxq), ('tokenPriFlag', 'rx'), ('p_rx_id', '%s;%s;%s;' % (xnxq, course_number, serial_number)), ('p_sort.asc1', 'true'), ('p_sort.asc2', 'true')),
-        'xwk': (('m', 'saveXwKc'), ('token', token_string), ('p_xnxq', xnxq), ('tokenPriFlag', 'xwk'), ('p_xwk_id', '%s;%s;%s;' % (xnxq, course_number, serial_number)))
-    }
-    course_req = urllib2.Request(base_url, urllib.urlencode(course_body[course_type]))
+    body = (('m', 'deleteYxk'), ('token', token_string), ('p_xnxq', xnxq), ('tokenPriFlag', 'yx'), ('p_del_id', '%s;%s;%s;%s' % (xnxq, course_number, serial_number, studentid)))
+    course_req = urllib2.Request(base_url, urllib.urlencode(body))
     course_req_html = urllib2.urlopen(course_req).read()
     # f_out = open(course_type + '_req.html', 'w')
     # f_out.write(course_req _html)
     # f_out.close()
-
-    return is_rob_success(base_url, course_number, serial_number)
 
 def rob_courses(courses, interval=1):
     result = [False for course in courses]
@@ -102,7 +176,10 @@ def rob_courses(courses, interval=1):
 
                 for idx, course in enumerate(courses):
                     if not result[idx]:
-                        result[idx] = course_rob(course[0], course[1], course[2])
+                        if len(course) == 3:
+                            result[idx] = rob_course(course[0], course[1], course[2])
+                        elif len(course) == 6:
+                            rob_course_with_swap(course[0], course[1], course[2], course[3], course[4], course[5])
                         time.sleep(interval)
             except:
                 break
